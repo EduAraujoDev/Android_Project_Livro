8fc973316d1163f50bd9c3d04ec6ef518461aa7c
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-02-27 19:00:24 -0300

    Commit inicial

---
+++ app/src/main/java/com/livroandroid/android_project_livro/MainActivity.java
@@ -0,0 +1,13 @@
+package com.livroandroid.android_project_livro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
