9d96977b6fdb72895f2fd79e804900b6e87bd1ea
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-02-28 13:32:12 -0300

    Criado as classes domain para Carro com Cardview

--- app/src/main/java/com/livroandroid/android_project_livro/fragments/CarrosFragment.java
+++ app/src/main/java/com/livroandroid/android_project_livro/fragments/CarrosFragment.java
@@ -1,15 +1,26 @@
 package com.livroandroid.android_project_livro.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.livroandroid.android_project_livro.R;
+import com.livroandroid.android_project_livro.adapter.CarroAdapter;
+import com.livroandroid.android_project_livro.domain.Carro;
+import com.livroandroid.android_project_livro.domain.CarroService;
+
+import java.util.List;
 
 public class CarrosFragment extends BaseFragment {
     private int tipo;
+    protected RecyclerView recyclerView;
+    private List<Carro> carros;
 
     // Método para instanciar esse fragment pelo tipo
     public static CarrosFragment newInstance(int tipo) {
@@ -35,9 +46,34 @@ public class CarrosFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_carros, container, false);
 
-        TextView textView = (TextView) view.findViewById(R.id.text);
-        textView.setText("Carros " + getString(tipo));
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setHasFixedSize(true);
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        taskCarros();
+    }
+
+    private void taskCarros() {
+        // Busca os carros
+        this.carros = CarroService.getCarros(getContext(), tipo);
+        // Atualiza a lista
+        recyclerView.setAdapter(new CarroAdapter(getContext(), carros, onClickCarro()));
+    }
+
+    private CarroAdapter.CarroOnClickListener onClickCarro() {
+        return new CarroAdapter.CarroOnClickListener() {
+            @Override
+            public void onClickCarro(View view, int idx) {
+                Carro c = carros.get(idx);
+                toast("Carro: " + c.nome);
+            }
+        };
     }
 }
