0b867660c6b9c44bbf1a3be7e3d6c5cf0bfa430e
Eduardo Araujo <eduardo.araujo0@outlook.com>
2017-02-28 12:44:14 -0300

    Webview no fragment Site do livro

--- app/src/main/java/com/livroandroid/android_project_livro/activity/MainActivity.java
+++ app/src/main/java/com/livroandroid/android_project_livro/activity/MainActivity.java
@@ -1,8 +1,11 @@
 package com.livroandroid.android_project_livro.activity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.livroandroid.android_project_livro.R;
+import com.livroandroid.android_project_livro.fragments.AboutDialog;
 import com.livroandroid.android_project_livro.fragments.CarrosFragment;
 
 public class MainActivity extends BaseActivity {
@@ -18,5 +21,22 @@ public class MainActivity extends BaseActivity {
 
         // Inicializa o layout principal com o fragment dos carros
         replaceFragment(CarrosFragment.newInstance(R.string.carros));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            AboutDialog.showAbout(getSupportFragmentManager());
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
